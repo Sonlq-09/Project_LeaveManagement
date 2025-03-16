8368a727c46681420c78cfb621ded8ecf16eadc2
Sonlq-09 <son159a@gmail.com>
2025-03-16 12:56:56 +0700

    Tạo LogoutServlet.java để xử lý đăng xuất
    - Tạo LogoutServlet.java trong pk Leave với @WebServlet("/logout").
    - Triển khai phương thức doGet() để hủy session hiện tại sẽ chấm dứt trạng thái đăng nhập.
    - Chuyển hướng người dùng về trang login.html sau khi đăng xuất.
    - Servlet này xử lý logic đăng xuất cho hệ thống này.

---
+++ Project_PRJ/src/java/Leave/LogoutServlet.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Leave;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LTSon
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("login.html");
+    }
+}
